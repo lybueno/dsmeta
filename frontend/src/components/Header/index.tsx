9ba06a574b393d7d4610464f9ff0f35991896c2f
Lygia Bueno <lybueno@gmail.com>
2022-07-17 00:01:15 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import logo from '../../assets/img/logo.svg'
+
+import './styles.css'
+
+function Header() {
+    return(
+        <div className="dsmeta-logo-container">
+            <img src={logo} alt="Logo DSMeta" />
+            <h1>DSMeta</h1>
+            <p>Desenvolvido por <a href="https://github.com/lybueno">/lybueno</a></p>   
+        </div>
+    )
+}
+
+export default Header
